8a7c0e77b8a5f333f2fd6cfce7baf742af5d4f5f
IAngelIi <dauletmaulet49@gmail.com>
2025-07-17 17:28:31 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$windows = "filename"; //zip
-$android = "filename"; //apk
-$tools = "toolspage";
+$windows = "https://wdfiles.ru/1u59o"; //zip
+$android = "https://wdfiles.ru/1u59m"; //apk
+$tools = "https://shadownix.ps.fhgdps.com/dashboard/";
 $demonlist = "demonlist";
-$youtube = "youtubeprofile";
-$discord = "discordserver";
+$youtube = "https://youtube.com/@bawmisto?si=nY6sfsE2wPXMsplW";
+$discord = "https://discord.gg/mH6mnZWgY9";
 $about = "Я Глава IT-части ShadownixTeam заявляю что я и мой дорогой друг Nvm(Передаю ему привет) унизили самого Аутистического Соовнера в мире имя ему - sireny.";
 ?>
