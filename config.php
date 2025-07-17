c319086f09ddf7cfc7f8ce6bb069852dd2dff5a0
IAngelIi <dauletmaulet49@gmail.com>
2025-07-17 17:25:51 +0500

    Update config.php

--- config.php
+++ config.php
@@ -5,5 +5,5 @@ $tools = "toolspage";
 $demonlist = "demonlist";
 $youtube = "youtubeprofile";
 $discord = "discordserver";
-$about = "Я Глава IT-части ShadownixTeam заявляю что я и мой дорогой друг Nvm(Передаю ему привет) унизили самого хуесосущего Соовнера в мире имя ему - sireny.";
+$about = "Я Глава IT-части ShadownixTeam заявляю что я и мой дорогой друг Nvm(Передаю ему привет) унизили самого Аутистического Соовнера в мире имя ему - sireny.";
 ?>
